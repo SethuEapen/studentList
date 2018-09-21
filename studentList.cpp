0c466544bfe94c7d0131142f53cd72054a5e8c43
U-BSD\357538 <357538@SHS-10L74C39B.beaverton.k12.or.us>
2018-09-21 08:40:22 -0700

    INIT Commit

---
+++ studentList.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Student {
+  char firstName[10];
+  char lastName[10];
+  int id;
+  float gpa;
+};
+
+int main()
+{
+  bool exit = false;
+  
+  vector<Student*> students;
+  
+  
+}
