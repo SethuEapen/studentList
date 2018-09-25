c215213bc17705ed86c36d4d6218d147335c1a56
SethuEapen <SethuEapen@gmail.com>
2018-09-25 09:11:39 -0700

    Second Commit

--- studentList.cpp
+++ studentList.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+void ADD(vector<Student*> students);
+void PRINT(vector<Student*> students);
+void DELETE(vector<Student*> students);
+
 struct Student {
   char firstName[10];
   char lastName[10];
@@ -13,8 +17,37 @@ struct Student {
 int main()
 {
   bool exit = false;
-  
+  char input[6];
   vector<Student*> students;
-  
-  
+  while(!exit){
+    cin.get(input, 6);
+    cin.clear();
+    cin.ignore(100000, '\n');
+    if(strcmp(input, "add") == 0){
+      ADD(students);
+    }
+    else if(strcmp(input, "print") == 0){
+      PRINT(students);
+    }
+    else if(strcmp(input, "delete") == 0){
+      DELETE(students);
+    }
+  }
+}
+
+void ADD(vector<Student*> students){
+  Student newStud;
+  cin >> newStud.firstName;
+  cin >> newStud.lastName;
+  cin >> newStud.id;
+  cin >> newStud.gpa;
+  students.push_back(newStud);
+}
+
+void PRINT(vector<Student*> students){
+
+}
+
+void DELETE(vector<Student*> students){
+
 }
